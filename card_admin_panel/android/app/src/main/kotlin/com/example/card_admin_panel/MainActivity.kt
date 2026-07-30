04bef6f2b85e52af8a615fb88e265cd1cdecea76
magedgasem55558-creator <magedgasem55558@gmail.com>
2026-07-30 04:17:51 +0300

    Delete card_admin_panel directory

--- card_admin_panel/android/app/src/main/kotlin/com/example/card_admin_panel/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.card_admin_panel
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- card_admin_panel/lib/services/role_service.dart
+++
@@ -1,8 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class RoleService {
-  Future<bool> checkAdmin(String uid) async {
-    final doc = await FirebaseFirestore.instance.collection('users').doc(uid).get();
-    return doc.exists && doc.data()?['role'] == 'admin';
-  }
-}
